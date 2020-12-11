2b67bee48e99548676225b5cd7bf808a24e63e96
Bishesh <bishesh.sunam@gmail.com>
2020-12-11 23:16:38 +1300

    fetched tweets to front end

--- js/server.js
+++ js/server.js
@@ -1,13 +1,16 @@
-const URL = "http://localhost:3000/tweets";
+const URL = "http://localhost:5000/tweets";
 
 /**
  * Retrive Twitter Data from API
  */
 const getTwitterData = () => {
     const url = "http://localhost:5000/tweets?q=coding&count=10";
-    fetch(url).then((response)=>{
-        return response;
-    }).then((data)=>{
+    fetch(url)
+    .then((response)=>{
+       
+       return response.json();
+    })
+    .then((data)=>{
       console.log(data); 
     })
 }
